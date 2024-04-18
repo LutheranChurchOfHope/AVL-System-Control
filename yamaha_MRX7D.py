4336a79542f59b63fc76d204e00e6f08e32ff4e2
Will Pile <willpile@users.noreply.github.com>
2024-04-18 03:24:48 -0500

    Update yamaha_MRX7D.py

--- yamaha_MRX7D.py
+++ yamaha_MRX7D.py
@@ -14,7 +14,7 @@ s = socket.socket(socket.AV_INET, socket.SOCK_STREAM)
 
 #in my testing using netcat it appears that the MRX7D would only receive and respond to commands after devmode normal request is first sent
 
-devstatus = b'devmode normal\n'
+devstatus = b'devstatus runmode\n'
 sampleoff = b'set MTX:Index_1 0 0 \"OFF\"\n'
 
 s.connect((TCP, TCP_PORT))
